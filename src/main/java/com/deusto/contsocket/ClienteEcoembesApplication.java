51508811b03f1a57634f109e374f1f75f67815a0
Jon Ander Areizaga <jonander.areizaga@opendeusto.es>
2025-12-04 12:56:17 +0100

    creación del Cliente Ecoembes

---
+++ src/main/java/com/deusto/contsocket/ClienteEcoembesApplication.java
@@ -0,0 +1,13 @@
+package com.deusto.contsocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClienteEcoembesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClienteEcoembesApplication.class, args);
+	}
+
+}
